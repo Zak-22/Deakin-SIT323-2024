7255bbf89f3975976d4dc013e8c9f121dfeab10c
Zak-22 <zako_322@hotmail.com>
2024-03-12 18:41:06 +1100

    Added Week 2 folder and completed Task 2.1P

---
+++ Week-2/server.js
@@ -0,0 +1,9 @@
+// Create variable express that holds the package name
+var express = require("express")
+// Create variable for 'express' package
+var app = express()
+// Sets the port variable value to 3000 and starts listening on this port and outputs this process.
+var port = process.env.port || 3000;
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
